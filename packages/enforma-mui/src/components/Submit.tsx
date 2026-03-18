bf3b7a38eae31fc473ce99084ea4f2571875f8e6
jacarma <jacarma@gmail.com>
2026-03-18 23:32:38 +0100

    fix: Wrap submit for style and coherence

--- packages/enforma-mui/src/components/Submit.tsx
+++ packages/enforma-mui/src/components/Submit.tsx
@@ -1,10 +1,13 @@
 import { Button } from '@mui/material';
 import type { ResolvedSubmitProps } from 'enforma';
+import { ComponentWrap } from './ComponentWrap.js';
 
 export function Submit({ children, disabled }: ResolvedSubmitProps) {
   return (
-    <Button type="submit" variant="contained" disabled={disabled ?? false}>
-      {children}
-    </Button>
+    <ComponentWrap>
+      <Button type="submit" variant="contained" disabled={disabled ?? false}>
+        {children}
+      </Button>
+    </ComponentWrap>
   );
 }
